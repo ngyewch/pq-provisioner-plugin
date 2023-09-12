9fe9dba12e39e01b2943bc3cfaafce9b61a9730d
Nick Ng <ngyewch@gmail.com>
2023-09-12 11:37:14 +0800

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# pq-provisioner-plugin
+<div align="center">
+
+# pq-provisioner-plugin [![Build](https://github.com/ngyewch/pq-provisioner-plugin/actions/workflows/build.yml/badge.svg)](https://github.com/ngyewch/pq-provisioner-plugin/actions/workflows/build.yml) [![Lint](https://github.com/ngyewch/pq-provisioner-plugin/actions/workflows/lint.yml/badge.svg)](https://github.com/ngyewch/pq-provisioner-plugin/actions/workflows/lint.yml)
+
+
+[pq-provisioner](https://github.com/ngyewch/pq-provisioner) plugin for the [asdf version manager](https://asdf-vm.com).
+
+</div>
+
+# Contents
+
+- [Dependencies](#dependencies)
+- [Install](#install)
+- [Contributing](#contributing)
+- [License](#license)
+
+# Dependencies
+
+- `bash`, `curl`, `tar`, `unzip`, `git`
+
+# Install
+
+Plugin:
+
+```shell
+asdf plugin add pq-provisioner https://github.com/ngyewch/pq-provisioner-plugin.git
+```
+
+pq-provisioner:
+
+```shell
+# Show all installable versions
+asdf list-all pq-provisioner
+
+# Install specific version
+asdf install pq-provisioner latest
+
+# Set a version globally (on your ~/.tool-versions file)
+asdf global pq-provisioner latest
+
+# Now pq-provisioner commands are available
+pq-provisioner version
+```
+
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install & manage versions.
+
+# Contributing
+
+Contributions of any kind welcome! See the [contributing guide](contributing.md).
+
+[Thanks goes to these contributors](https://github.com/ngyewch/pq-provisioner-plugin/graphs/contributors)!
+
+# License
+
+See [LICENSE](LICENSE) © [Nick Ng](https://github.com/ngyewch/)
---
+++ lib/utils.bash
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/ngyewch/pq-provisioner"
+TOOL_NAME="pq-provisioner"
+TOOL_TEST="pq-provisioner version"
+
+fail() {
+  echo -e "asdf-$TOOL_NAME: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+# NOTE: You might want to remove this if pq-provisioner is not hosted on GitHub releases.
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+}
+
+list_all_versions() {
+  list_github_tags
+}
+
+get_arch() {
+  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+  case ${arch} in
+  arm64)
+    arch='arm64'
+    ;;
+  arm6)
+    arch='arm6'
+    ;;
+  x86_64)
+    arch='amd64'
+    ;;
+  aarch64)
+    arch='arm64'
+    ;;
+  i386)
+    arch='i386'
+    ;;
+  esac
+
+  echo ${arch}
+}
+
+get_platform() {
+  plat=$(uname | tr '[:upper:]' '[:lower:]')
+  case ${plat} in
+  darwin)
+    plat='darwin'
+    ;;
+  linux)
+    plat='linux'
+    ;;
+  windows)
+    plat='windows'
+    ;;
+  esac
+
+  echo ${plat}
+}
+
+get_ext() {
+  plat=$(uname | tr '[:upper:]' '[:lower:]')
+  case ${plat} in
+  darwin)
+    ext='zip'
+    ;;
+  linux)
+    ext='zip'
+    ;;
+  windows)
+    ext='zip'
+    ;;
+  *)
+    ext='zip'
+    ;;
+  esac
+
+  echo ${ext}
+}
+
+download_release() {
+  local version filename url
+  version="$1"
+  filename="$2"
+
+  url="$GH_REPO/releases/download/v${version}/pq-provisioner_${version}_$(get_platform)_$(get_arch).$(get_ext)"
+
+  echo "* Downloading $TOOL_NAME release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="${3%/bin}/bin"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
+
+  (
+    mkdir -p "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+
+    # TODO: Assert pq-provisioner executable exists.
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
+}
